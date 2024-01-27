3eb7b0e99507e0bc1b2399914dcccac431a38010
Jordan Keen <jordankeen.7@gmail.com>
2024-01-27 10:46:34 -0500

    setup shared class styles for icons and buttons

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import { faHome, faTicket } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 
 const Nav = () => {
    return (
-      <div>
-         Nav
-      </div>
+      <nav>
+         <div>
+            <Link href="/">
+               <FontAwesomeIcon icon={faHome} className='icon' />
+            </Link>
+            <Link href="/TicketPage/new">
+               <FontAwesomeIcon icon={faTicket} className='icon' />
+            </Link>
+         </div>
+         <div>
+            <p className='text-default-text'>user@email.com</p>
+         </div>
+      </nav>
    )
 }
 
