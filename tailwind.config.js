661036161662edecb19f7e9560fb0f359c73fe63
Jordan Keen <jordankeen.7@gmail.com>
2024-01-27 11:39:45 -0500

    edited styling of ticketcard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
             'gradient-conic':
                'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
          },
+         // use quotes "" for multi word declarations
          colors: {
             nav: "#18222f",
             page: "#2b3441",
