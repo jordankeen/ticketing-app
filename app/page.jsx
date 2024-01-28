934f7b487e0ad01e232717ed41f5b1fbdedf2cb7
Jordan Keen <jordankeen.7@gmail.com>
2024-01-28 16:26:38 -0500

    create getTickets function for dashboard page, build out render functionality for unique categories and tickets

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,40 @@
 import TicketCard from "./(components)/TicketCard"
 
-const Dashboard = () => {
+const getTickets = async () => {
+   try {
+      const res = await fetch('http://localhost:3000/api/Tickets', {
+         cache: 'no-store'
+      })
+
+      return res.json();
+   } catch (error) {
+      console.log('Failed to get tickets.', error);
+   }
+}
+
+const Dashboard = async () => {
+
+   const { tickets } = await getTickets();
+
+   // get all categories
+   const uniqueCategories = [
+      ... new Set(tickets?.map(({ category }) => category)),
+   ];
+
+   // render each unique category with filtered tickets
    return (
       <div className="p-5">
-         <div className="md:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            <TicketCard />
-            <TicketCard />
-            <TicketCard />
-            <TicketCard />
+         <div>
+            {tickets && uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+               <div className="mb-4" key={categoryIndex}>
+                  <h2>{uniqueCategory}</h2>
+                  <div className="md:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                     {tickets.filter((ticket) => ticket.category === uniqueCategory).map((ticket, _index) => (
+                        <TicketCard key={_index} id={_index} ticket={ticket} />
+                     ))}
+                  </div>
+               </div>
+            ))}
          </div>
       </div>
    )
