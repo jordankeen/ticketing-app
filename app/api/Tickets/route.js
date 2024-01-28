41b2959b2941d7b677fc036e9129ffa039fa3b54
Jordan Keen <jordankeen.7@gmail.com>
2024-01-28 16:24:16 -0500

    create route for getting all tickets

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -15,3 +15,13 @@ export async function POST(req) {
       return NextResponse.json({ message: "Error", error }, { status: 500 });
    }
 }
+
+// Get all tickets
+export async function GET() {
+   try {
+      const tickets = await Ticket.find();
+      return NextResponse.json({ tickets }, { status: 200 });
+   } catch (error) {
+      return NextResponse.json({ message: "Error", error }, { status: 500 });
+   }
+}
