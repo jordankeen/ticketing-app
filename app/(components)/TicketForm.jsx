c73082d94ebe69b308a72401119f6baba46c1168
Jordan Keen <jordankeen.7@gmail.com>
2024-01-28 15:45:14 -0500

    added all html for form inputs

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -67,7 +67,7 @@ const TicketForm = () => {
                <option value="Software Problem">Software Problem</option>
                <option value="Project">Project</option>
             </select>
-            <label htmlFor="priority">Priority</label>
+            <label>Priority</label>
             <div>
                <input
                   id="priority-1"
@@ -78,7 +78,69 @@ const TicketForm = () => {
                   checked={formData.priority == 1}
                />
                <label>1</label>
+               <input
+                  id="priority-2"
+                  name="priority"
+                  type="radio"
+                  onChange={handleChange}
+                  value={2}
+                  checked={formData.priority == 2}
+               />
+               <label>2</label>
+               <input
+                  id="priority-3"
+                  name="priority"
+                  type="radio"
+                  onChange={handleChange}
+                  value={3}
+                  checked={formData.priority == 3}
+               />
+               <label>3</label>
+               <input
+                  id="priority-4"
+                  name="priority"
+                  type="radio"
+                  onChange={handleChange}
+                  value={4}
+                  checked={formData.priority == 4}
+               />
+               <label>4</label>
+               <input
+                  id="priority-5"
+                  name="priority"
+                  type="radio"
+                  onChange={handleChange}
+                  value={5}
+                  checked={formData.priority == 5}
+               />
+               <label>5</label>
             </div>
+            <label htmlFor="progress">Progress</label>
+            <input
+               type="range"
+               id="progress"
+               name="progress"
+               value={formData.progress}
+               min="0"
+               max="100"
+               onChange={handleChange}
+            />
+            <label htmlFor="status">Status</label>
+            <select
+               name="status"
+               id="status"
+               value={formData.status}
+               onChange={handleChange}
+            >
+               <option value="not started">Not Started</option>
+               <option value="started">Started</option>
+               <option value="done">Done</option>
+            </select>
+            <input
+               type="submit"
+               className="btn max-w-xs"
+               value="Create Ticket"
+            />
          </form>
       </div>
    );
